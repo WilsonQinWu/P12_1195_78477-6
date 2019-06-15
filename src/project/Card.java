08df32b37df897b2a3eb6bf310cb147a2360d1d3
ZhongyaZhang0624 <50380378+ZhongyaZhang0624@users.noreply.github.com>
2019-06-15 13:10:30 -0400

    Card_version0.9

--- src/project/Card.java
+++ src/project/Card.java
@@ -11,7 +11,7 @@ package project;
  * should remember to add themselves as a modifier.
  *
  * @author megha, 2019
- * version 0.8
+ * version 0.9
  */
 public class Card
 {
@@ -22,7 +22,7 @@ public class Card
    };
    public enum Value
    {
-      Ace, Two, Three, Four, Five, Six, Eight, Nine, Ten, Jack, Queen, King
+      Ace, Two, Three, Four, Five, Six, Eight, Nine, Ten, Eleven，Jack, Queen, King
    };
 
    private final Suit suit;
