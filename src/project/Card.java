086cd7bc4f78d4c7ee1535395adbab5adc8004de
zhongya <zhongya@sheridancollege.ca>
2019-07-19 16:07:06 -0400

    change the Card class

--- src/project/Card.java
+++ src/project/Card.java
@@ -7,21 +7,36 @@ package project;
 
 /**
  * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
+ * enough to be instantiated for any Card game. Students wishing to add to the
+ * code should remember to add themselves as a modifier.
+ *
  * @author megha, 2019
  */
 public class Card {
 
-    public enum Suit{Hearts,Spades,Diamands,Clubs};
-    public enum Value{Ace,Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King};
-    
+    public enum Suit {
+        Hearts, Spades, Diamands, Clubs
+    };
+
+    public enum Value {
+        Ace(1), Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8), Nine(9), Ten(10), Jack(11), Queen(12), King(13);
+        private int value;
+
+        public int getValue() {
+            return value;
+        }
+
+        private Value(int value) {
+            this.value = value;
+        }
+    }
+
     private final Suit suit;
     private final Value value;
-    
-    Card(Suit s, Value v){
-    suit=s;
-    value=v;
+
+    Card(Suit s, Value v) {
+        suit = s;
+        value = v;
     }
 
     /**
